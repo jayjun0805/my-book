110ee541a1d92b77496878bfa4fd08765bfac3f5
jayjun0805 <jayjun0805@sina.com>
2019-11-16 10:48:02 +0800

    add init code

--- 简易计算器/pages/jsq/jsq.js
+++ 简易计算器/pages/jsq/jsq.js
@@ -1,4 +1,5 @@
 // pages/jsq/jsq.js
+
 Page({
 
   /**
@@ -8,13 +9,12 @@ Page({
     numberOne: 0,  	// 第一个数的值，默认为0
     numberTwo: 0,  	// 第二个数的值，默认为0
     operator: "+", 	// 运算符，默认为+
-    result: 0 		// 计算结果，默认为0
+    result: 0 		  // 计算结果，默认为0 
   },
-
   // 第一个数的键盘输入时触发input事件
-  numberOneInput: function(e){
-    // console.log(e.detail.value);
-    //更新第一个数的值
+  numberOneInput: function (e) {
+    console.log(e.detail.value);
+    // 更新第一个数的值
     this.setData({
       numberOne: e.detail.value
     })
@@ -22,16 +22,16 @@ Page({
 
   // 第二个数的键盘输入时触发input事件
   numberTwoInput: function (e) {
-    // console.log(e.detail.value);
-    //更新第二个数的值
+    console.log(e.detail.value);
+    // 更新第二个数的值
     this.setData({
       numberTwo: e.detail.value
     })
   },
 
   // 单项选择器的选中项发生变化时触发 change 事件
-  radioGroupChange: function(e){
-    // console.log(e.detail.value);
+  radioGroupChange: function (e) {
+    console.log(e.detail.value);
     //更新运算符的值
     this.setData({
       operator: e.detail.value
@@ -39,17 +39,16 @@ Page({
   },
 
   // 开始计算手指触摸离开 tap 事件
-  startBtnTap: function(e){
-    // console.log(e);
+  startBtnTap: function (e) {
+    console.log(e);
     // 1、获取第一个数的值、第二个数的值和单项选择器的选中项的值
-    var numberOne = parseFloat(this.data.numberOne);
-    var numberTwo = parseFloat(this.data.numberTwo);
-    var operator = this.data.operator;
+    let numberOne = parseFloat(this.data.numberOne);
+    let numberTwo = parseFloat(this.data.numberTwo);
+    let operator = this.data.operator;
 
     // 2、根据单项选择器的选中项的值进行判断，写一个switch 语句，完成计算
-    var result = 0;
-    switch(operator)
-    {
+    let result = 0;
+    switch (operator) {
       case "+":
         result = numberOne + numberTwo;
         break;
@@ -66,7 +65,7 @@ Page({
 
     // 3、将计算结果显示，使用setData更新result数据
     this.setData({
-      result: result
+      result
     })
   },
 
@@ -74,55 +73,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
